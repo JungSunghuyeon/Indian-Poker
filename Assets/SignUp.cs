2d25e7f3075757f4351fc06cf4eb00a12179af50
JungSunghuyeon <51109510+JungSunghuyeon@users.noreply.github.com>
2021-05-05 12:55:48 +0900

    Jung_1weeks

---
+++ Assets/SignUp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SignUp: MonoBehaviour
+{
+    public void Change() {
+        SceneManager.LoadScene("SignUp");
+    }
+}
